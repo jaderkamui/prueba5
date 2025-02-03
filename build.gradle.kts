d66940f304f10401df76e696ae43a1dc47dfd927
jaderkamui <tecnico.jader@gmail.com>
2025-02-03 18:07:51 -0300

    prueba

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,18 +1,7 @@
-pluginManagement {
-    repositories {
-        google()
-        mavenCentral()
-        gradlePluginPortal()
-    }
+// Top-level build file where you can add configuration options common to all sub-projects/modules.
+plugins {
+    id("com.android.application") version "8.1.0" apply false
+    id("org.jetbrains.kotlin.android") version "1.9.24" apply false
+    // Add the Compose Compiler plugin here
+    id("org.jetbrains.kotlin.plugin.compose") version "2.0.0" apply false
 }
-dependencyResolutionManagement {
-    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
-    repositories {
-        google()
-        mavenCentral()
-        maven { url = uri("https://jitpack.io") }
-    }
-}
-
-rootProject.name = "jader_munoz_seccioncurso5"
-include(":app")
