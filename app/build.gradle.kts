76d3f567021d89ae6600a8c1f0cd5d0c3c1cfcd6
jaderkamui <tecnico.jader@gmail.com>
2025-02-03 18:09:21 -0300

    prueba

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
-    id("com.android.application")
-    id("org.jetbrains.kotlin.android")
+    id("com.android.application") version "8.1.0" apply false
+    id("org.jetbrains.kotlin.android") version "1.9.24" apply false
+    id("org.jetbrains.kotlin.plugin.compose") version "1.9.24" apply false
 }
 
 android {
